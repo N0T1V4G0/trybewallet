8eb10fb256933d8d1ed5d3a2e2cc75104ea94393
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-01-11 21:18:27 -0300

    add req 3

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,12 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SET_USER_EMAIL } from '../actions';
+
 const INITIAL_STATE = {
   email: '',
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
-  if (action.type === 'SET_USER_EMAIL') {
+  if (action.type === SET_USER_EMAIL) {
     return {
       ...state,
       email: action.payload,
